d1cf0567d295bd8825c3a6dd5f7c659463c18ffc
danieldaga <daniel@arhrestauracion.com>
2022-11-14 21:24:34 +0100

    final de clase

---
+++ main.js
@@ -0,0 +1,41 @@
+const button1 = document.querySelector('#buttonOne')
+const button2 = document.querySelector('#buttonTwo')
+const button3 = document.querySelector('#buttonThree')
+const button4 = document.querySelector('#buttonFour')
+const button5 = document.querySelector('#buttonFive')
+const button6 = document.querySelector('#buttonSix')
+const list = document.querySelector('#list')
+const title = document.querySelector('#titleOne')
+// list.style.backgroundColor = "grey"
+// title.style.color = "green"
+
+listUl.style.listStyle = "none"
+list.style.borderRadius = '30px'
+listUl.style.textAlign = 'center'
+title.style.textAlign = 'center'
+console.log({ buttonOne })
+
+button1.onclick = () => {
+    title.style.color = "green"
+    console.log(buttonOne)
+}
+button2.onclick = () => {
+    title.style.color = "green"
+    console.log(buttonOne)
+}
+button3.onclick = () => {
+    title.style.color = "green"
+    console.log(buttonOne)
+}
+button4.onclick = () => {
+    title.style.color = "green"
+    console.log(buttonOne)
+}
+button5.onclick = () => {
+    title.style.color = "green"
+    console.log(buttonOne)
+}
+button6.onclick = () => {
+    title.style.color = "green"
+    console.log(buttonOne)
+}
