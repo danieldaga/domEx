62de2d90d90e09a302cb31c424ae401a0967fab2
danieldaga <daniel@arhrestauracion.com>
2022-11-15 12:23:43 +0100

    done

--- main.js
+++ main.js
@@ -6,36 +6,55 @@ const button5 = document.querySelector('#buttonFive')
 const button6 = document.querySelector('#buttonSix')
 const list = document.querySelector('#list')
 const title = document.querySelector('#titleOne')
-// list.style.backgroundColor = "grey"
+// listUl.style.backgroundColor = "grey"
 // title.style.color = "green"
 
 listUl.style.listStyle = "none"
-list.style.borderRadius = '30px'
+// list.style.borderRadius = '30px'
 listUl.style.textAlign = 'center'
 title.style.textAlign = 'center'
 console.log({ buttonOne })
 
+
 button1.onclick = () => {
-    title.style.color = "green"
-    console.log(buttonOne)
+    if (title.style.color === "green") {
+        title.style.color = ""
+    } else {
+        title.style.color = "green"
+    }
 }
 button2.onclick = () => {
-    title.style.color = "green"
-    console.log(buttonOne)
+    if (button2.style.backgroundColor === "pink") {
+        button2.style.backgroundColor = "white"
+    } else {
+        button2.style.backgroundColor = "pink"
+    }
 }
 button3.onclick = () => {
-    title.style.color = "green"
-    console.log(buttonOne)
+    if (title.style.fontSize === "100px") {
+        title.style.fontSize = ""
+    } else {
+        title.style.fontSize = "100px"
+    }
 }
 button4.onclick = () => {
-    title.style.color = "green"
-    console.log(buttonOne)
+    if (button4.style.backgroundColor === "blue") {
+        button4.style.backgroundColor = ""
+    } else {
+        button4.style.backgroundColor = "blue"
+    }
 }
 button5.onclick = () => {
-    title.style.color = "green"
-    console.log(buttonOne)
+    if (button5.style.backgroundColor === "white") {
+        button5.style.backgroundColor = ""
+    } else {
+        button5.style.backgroundColor = "white"
+    }
 }
 button6.onclick = () => {
-    title.style.color = "green"
-    console.log(buttonOne)
+    if (title.style.textTransform === 'uppercase') {
+        title.style.textTransform = 'lowercase'
+    } else {
+        title.style.textTransform = 'uppercase'
+    }
 }
